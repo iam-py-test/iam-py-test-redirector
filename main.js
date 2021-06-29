29c22e4569d10e4782e72cdea4263f9203a2f1f5
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-29 15:49:45 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+var url = new URL(location).searchParams.get('url')
+var token = new URL(location).searchParams.get('token')
+document.getElementById('url').textContent = url
+try{
+var host = (new URL(url).hostname||exitvar)
+catch(err){
+  document.getElementById('url').textContent = "Invalid url"
+  var host = null
+}
