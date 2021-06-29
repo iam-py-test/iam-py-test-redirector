12cfba41925aaaeaec2ad4512bb48f55cfd76367
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-29 15:54:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var token = new URL(location).searchParams.get('token')
 document.getElementById('url').textContent = url
 try{
 var host = (new URL(url).hostname||exitvar)
+}
 catch(err){
   document.getElementById('url').textContent = "Invalid url"
   var host = null
